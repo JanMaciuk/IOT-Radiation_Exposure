5be23a78072e19c8dcbc9b6bf133f32911a08444
Jakub Śliwka <kubapplll@gmail.com>
2025-01-04 20:53:42 +0100

    feat: Add Web API elo

---
+++ RadiationExposure.API/IOT/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore; 
+
+namespace IOT.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Employee> Employees { get; set; }
+        public DbSet<Models.Zone> Zones { get; set; }
+        public DbSet<Models.EmployeesRadiation> EmployeesRadiations { get; set; }
+
+    }
+}
---
+++ RadiationExposure.API/IOT/Migrations/20250104193018_Init.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IOT.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Card = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Info = table.Column<string>(type: "text", nullable: false),
+                    Radiation = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeesRadiations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FkEmployee = table.Column<int>(type: "integer", nullable: false),
+                    FkZone = table.Column<int>(type: "integer", nullable: false),
+                    EntranceTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExitTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeesRadiations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeesRadiations_Employees_FkEmployee",
+                        column: x => x.FkEmployee,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeesRadiations_Zones_FkZone",
+                        column: x => x.FkZone,
+                        principalTable: "Zones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeesRadiations_FkEmployee",
+                table: "EmployeesRadiations",
+                column: "FkEmployee");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeesRadiations_FkZone",
+                table: "EmployeesRadiations",
+                column: "FkZone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeesRadiations");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Zones");
+        }
+    }
+}
