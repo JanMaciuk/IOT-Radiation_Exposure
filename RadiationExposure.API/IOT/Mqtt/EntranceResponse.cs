1781e86cfe8d331469c01c3ea499996a6ec0855b
Jedrzej <tcankon@gmail.com>
2025-01-20 15:09:35 +0100

    mqtt communication both ways on rasberry

---
+++ RadiationExposure.API/IOT/Mqtt/EntranceResponse.cs
@@ -0,0 +1,9 @@
+namespace IOT.Mqtt
+{
+    public class EntranceResponse
+    {
+        public string CardId { get; set; }
+        public int ZoneId ( get; set; )
+        public bool AccessGranted { get; set; }
+    }
+}
