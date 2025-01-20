a325405f8feb3970487410c6251244c2e7a0bcbd
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2025-01-20 17:50:35 +0100

    Raspberry PI reader station, implement MQTT

---
+++ RaspberryFiles/mqtt_client.py
@@ -0,0 +1,65 @@
+import paho.mqtt.client as mqtt
+import json
+import threading
+from datetime import datetime
+
+broker: str = "localhost"
+card_channel_name: str = "iot/entrance_logs"
+response_channel_name: str = "iot/entrance_response"
+
+card_access_responses = []
+# {
+#     "CardId": str,
+#     "ZoneId": int,
+#     "AccessGranted": bool
+# }
+
+lock = threading.Lock()
+condition = threading.Condition(lock)
+
+client = mqtt.Client()
+
+def process_message(client, userdata, message):
+    global card_access_responses
+    with condition:
+        print(f"Otrzymano wiadomość: {message.payload.decode()} na temat: {message.topic}")
+        card_access_responses.append(json.loads(message.payload.decode()))
+        condition.notify_all() 
+
+
+def wait_for_access_granted(cardId: str) -> bool:
+    def message_in_list():
+        return cardId in [item.get("CardId") for item in card_access_responses]
+
+    with condition:
+        if condition.wait_for(message_in_list, timeout=3):
+            for index, item in enumerate(card_access_responses):
+                if item.get('CardId') == cardId:
+                    access_granted_value = item.get('AccessGranted')
+                    del card_access_responses[index]  # Usuwanie słownika z listy
+                    return access_granted_value
+        return False
+
+def connect():
+    client.connect(broker)
+    print("connected")
+    client.on_message = process_message
+    client.subscribe(response_channel_name)
+    client.loop_start()
+
+def disconnect():
+    client.disconnect()
+
+
+def sendLog(card_id: str, zone_id: int):
+    entrance_log = {
+        "CardId": card_id,
+        "ZoneId": zone_id,
+        "Timestamp": datetime.now().isoformat()
+    }
+    client.publish(card_channel_name, json.dumps(entrance_log))
+
+if __name__ == "__main__":
+    connect()
+    sendLog("123123", 0)
+    disconnect()
