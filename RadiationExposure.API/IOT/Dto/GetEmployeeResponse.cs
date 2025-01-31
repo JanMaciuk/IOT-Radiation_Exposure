3bd8eded2be0f0e6746704fd251396311ee35b19
Jakub Śliwka <115873669+sliweq@users.noreply.github.com>
2025-01-31 18:42:57 +0100

    chore: Adjust project with yesterday changes

--- RadiationExposure.API/IOT/Dto/GetEmployeeResponse.cs
+++ RadiationExposure.API/IOT/Dto/GetEmployeeResponse.cs
@@ -2,12 +2,12 @@ namespace IOT.Dto;
 
 public class GetEmployeeResponse
 {
-	public required int Id { get; set; }
-	public required string Name { get; set; }
-	public required string Surname { get; set; }
-	public required double RadiationDoseInLastMonth { get; set; }
-	public required double RadiationDoseInLastWeek { get; set; }
-	public required DateTime? LastEntranceDate { get; set; }
-	public required int? LastZoneId { get; set; }
-	public required string? LastZoneName { get; set;}
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required double RadiationDoseInLastMonth { get; set; }
+    public required double RadiationDoseInLastWeek { get; set; }
+    public required DateTime? LastEntranceDate { get; set; }
+    public required int? LastZoneId { get; set; }
+    public required string? LastZoneName { get; set; }
 }
